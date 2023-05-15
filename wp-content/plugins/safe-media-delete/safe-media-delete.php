ad9d7df9b94583de7f0b577597d8b47eb5edd916
Ali imran <ali.imran@virtualforce.io>
2023-05-15 16:33:33 +0500

    Media Info API and Prevent Delete API

--- wp-content/plugins/safe-media-delete/safe-media-delete.php
+++ wp-content/plugins/safe-media-delete/safe-media-delete.php
@@ -234,7 +234,7 @@ add_filter('manage_media_columns', 'add_attached_objects_column');
             }
 
             if (!empty($post_links)) {
-                echo '<strong>Posts/Pages: </strong>' . implode(', ', $post_links) . '<br>';
+                echo '<strong>Posts: </strong>' . implode(', ', $post_links) . '<br>';
             }
 
             if ($object_type === 'term') {
@@ -252,7 +252,7 @@ add_filter('manage_media_columns', 'add_attached_objects_column');
                         
                         foreach ($term_edit_links as $taxonomy => $term_edit_link) {
                             if ($term_edit_link) {
-                                $term_links[] = '<a href="' . esc_url($term_edit_link) . '">' . $taxonomy . ': ' . $term . '</a>';
+                                $term_links[] = '<a href="' . esc_url($term_edit_link) . '">' . $term . '</a>';
                             }
                         }
                     }
@@ -272,15 +272,14 @@ add_action('manage_media_custom_column', 'populate_attached_objects_column', 10,
 
 
 // Enqueue custom script for the media modal
-function my_plugin_add_attached_objects_field_to_media_modal($form_fields, $attachment)
+ function my_plugin_add_attached_objects_field_to_media_modal($form_fields, $attachment)
 {
     $attached_objects = get_attached_objects($attachment->ID);
     $attached_terms = get_attached_terms($attachment->ID);
     
     if (!empty($attached_objects)) {
-        $html = '<div class="attachment-display-settings">';
-        $html .= '<label for="attached-objects">' . __('Attached Objects') . '</label><br/>';
-        $html .= '<span class="attached-object-ids">';
+        $html = '<div>';
+       
 
         $post_links = array();
         $term_links = array();
@@ -297,7 +296,7 @@ function my_plugin_add_attached_objects_field_to_media_modal($form_fields, $atta
         }
 
         if (!empty($post_links)) {
-            $html .= '<strong>Posts/Pages: </strong>' . implode(', ', $post_links) . '<br>';
+            $html .= '<strong>Posts: </strong>' . implode(', ', $post_links) . '<br>';
         }
 
         if ($object_type === 'term') {
@@ -312,7 +311,7 @@ function my_plugin_add_attached_objects_field_to_media_modal($form_fields, $atta
                     
                     foreach ($term_edit_links as $taxonomy => $term_edit_link) {
                         if ($term_edit_link) {
-                            $term_links[] = '<a href="' . esc_url($term_edit_link) . '">' . $taxonomy . ': ' . $term . '</a>';
+                            $term_links[] = '<a href="' . esc_url($term_edit_link) . '">' . $term . '</a>';
                         }
                     }
                 }
@@ -326,22 +325,23 @@ function my_plugin_add_attached_objects_field_to_media_modal($form_fields, $atta
             $html .= '<strong>Terms: </strong>' . implode(', ', $term_links);
         }
 
-        $html .= '</span>';
         $html .= '</div>';
 
         $form_fields['attached_objects'] = array(
-            'label' => '',
+            'label' => 'Attached Objects',
             'input' => 'html',
             'html'  => $html,
+            'required' => false, // Set required to false
         );
     }
+    $form_fields['attached_objects']['show_in_edit'] = false;
+    $form_fields['attached_objects']['show_in_modal'] = true;
 
     return $form_fields;
 }
 
 
 add_filter('attachment_fields_to_edit', 'my_plugin_add_attached_objects_field_to_media_modal', 10, 2);
-
 
 // Prevent Image Deletion for Media List View
 function prevent_image_deletion($delete, $post) {
@@ -419,3 +419,151 @@ add_filter('pre_delete_attachment', 'prevent_image_deletion', 10, 2);
 
 
 
+
+ 
+
+
+/* API */
+
+// Image Details API
+
+// Register REST API endpoints
+ function register_assignment_api_endpoints() {
+    register_rest_route( 'assignment/v1', '/media/(?P<id>\d+)', array(
+        'methods'  => 'GET',
+        'callback' => 'get_image_details',
+    ) );
+
+
+}
+add_action( 'rest_api_init', 'register_assignment_api_endpoints' );
+// Callback function for retrieving image details
+ function get_image_details( $request ) {
+    $image_id = $request->get_param( 'id' );
+    $image = get_post( $image_id );
+
+    if ( empty( $image ) || 'attachment' !== $image->post_type ) {
+        return new WP_Error( 'invalid_image', 'Invalid image ID', array( 'status' => 404 ) );
+    }
+
+    $image_details = array(
+        'ID'          => $image->ID,
+        'Date'        => $image->post_date,
+        'Slug'        => $image->post_name,
+        'Type'        => get_post_mime_type( $image->ID ),
+        'Link'        => wp_get_attachment_url( $image->ID ),
+        'Alt text'    => get_post_meta( $image->ID, '_wp_attachment_image_alt', true ),
+        'Attached Objects' => get_attached_objects_details( $image->ID ),
+    );
+
+    return $image_details;
+}
+
+function get_attached_objects_details( $image_id ) {
+    $attached_objects = get_attached_objects( $image_id );
+    $attached_objects_details = array();
+
+    foreach ( $attached_objects as $attached_object ) {
+        if ( 'post' === $attached_object['type'] ) {
+          
+            $attached_objects_details[] = array(
+                'ID'   => $attached_object['id'],
+                'Type' => 'post',
+            );
+        }
+        } foreach ( $attached_objects as $attached_object ) {
+            if ( 'term' === $attached_object['type'] ) {
+            $attached_terms = get_attached_terms( $image_id );
+            $term_objects_details = array();
+            
+            foreach ( $attached_terms as $attached_term ) {
+                $term_objects_details[] = array(
+                    'ID'   => $attached_term,
+                    'Type' => 'term',
+                );
+            }
+
+        }
+        }
+        if ( ! empty( $term_objects_details ) ) {
+            $attached_objects_details = array_merge( $attached_objects_details, $term_objects_details );
+        }
+    
+    
+    return $attached_objects_details;
+}
+
+
+
+// Prevent Media Delete API
+
+function delete_media_by_id($id) {
+    // Check if the attachment exists
+    $attachment = get_post($id);
+    if (!$attachment || $attachment->post_type !== 'attachment') {
+        return new WP_Error('media_not_found', 'Media not found.', array('status' => 404));
+    }
+
+    // Check if the attachment is attached as a featured image
+    $is_featured_image = get_posts(array(
+        'post_type'      => 'any',
+        'meta_key'       => '_thumbnail_id',
+        'meta_value'     => $id,
+        'posts_per_page' => 1,
+    ));
+
+    if ($is_featured_image) {
+        return new WP_Error('media_attached', 'Deletion failed. Media is set as a featured image.', array('status' => 400));
+    }
+
+    // Check if the attachment is used in the block editor
+
+    global $wpdb;
+    $query = $wpdb->prepare("
+        SELECT ID
+        FROM {$wpdb->posts}
+        WHERE post_content LIKE %s
+            AND post_type IN ('post', 'page')
+            AND post_status IN ('publish', 'draft', 'future', 'pending', 'private')
+    ", '%wp-image-' . $id . '%');
+    $is_block_editor_image = $wpdb->get_col($query);
+
+    if ($is_block_editor_image) {
+        return new WP_Error('media_attached', 'Deletion failed. Media is used in the block editor.', array('status' => 400));
+    }
+
+    // Check if the attachment is associated with CMB2 image fields in taxonomy terms
+    $is_cmb2_image_field_attachment = get_attached_terms($id);
+  
+    if ($is_cmb2_image_field_attachment) {
+        return new WP_Error('media_attached', 'Deletion failed. Media is associated with CMB2 image fields in taxonomy terms.', array('status' => 400));
+    }
+
+    // Delete the attachment
+    $result = wp_delete_attachment($id, true);
+    if ($result === false) {
+        return new WP_Error('media_deletion_failed', 'Failed to delete the media.', array('status' => 500));
+    }
+
+    // Media deleted successfully
+    return new WP_REST_Response(array('message' => 'Media deleted successfully.'), 200);
+}
+
+// Register the custom REST API route
+function register_delete_media_route() {
+    register_rest_route('assignment/v1', '/media/(?P<id>\d+)/delete', array(
+        'methods' => 'GET',
+        'callback' => 'handle_delete_media_request',
+    ));
+}
+add_action('rest_api_init', 'register_delete_media_route');
+
+// Handle the POST request to delete the media
+function handle_delete_media_request($request) {
+    $media_id = $request->get_param('id');
+
+    // Delete the media
+    $response = delete_media_by_id($media_id);
+    return $response;
+} 
+/* API */
